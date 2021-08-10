9a36dd5c26ec198f205d037491c736c18d5e416a
Kestrad <firedreamer@gmail.com>
2021-08-10 05:10:07 -0700

    Save and display ads

--- src/App.js
+++ src/App.js
@@ -58,9 +58,6 @@ function App() {
     let ads = allAds.slice();
     ads.push(fields);
     setAllAds(ads);
-    return (
-      <div>Ads will show up here</div>
-    )
   }
 
   function submit() {
@@ -70,18 +67,39 @@ function App() {
     setFormFields({});
   }
 
+  function showAd(fields) {
+    return (
+        <div className="Ad">
+            <h1>{fields.InitBird.Name}</h1>
+            <div className="info">
+                <div className="PreviewImage">
+                    <img src={fields.MiddleBird.Image} />
+                </div>
+                Sex: {fields.InitBird.Sex}
+                <br />
+                How much more adorable than cute: {fields.MiddleBird.Cuteness}%
+                <br />
+                Type of friend: {fields.MiddleBird.Friendtype}
+                <br />
+                Needs to leave before {fields.MiddleBird.Date}
+            </div>
+        </div>
+    )
+}
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Welcome to Birblist!</h1>
       </header>
       <button className="Start" onClick={openModal}>List a birb</button>
+      {allAds.map((ad) => showAd(ad))}
       <Modal visible={modalVisibility} 
              next={(modalFrame < numFrames-1) ? nextFrame : null} 
              back={(modalFrame > 0) ? prevFrame : null}
              submit={(modalFrame === numFrames-1 && allowSubmit) ? submit : null} 
              close={closeModal}>
-        <Frame saveFields={saveFormFields} values={formFields} setAllowSubmit={setAllowSubmit} />
+        <Frame saveFields={saveFormFields} values={formFields} setAllowSubmit={setAllowSubmit} showAd={showAd} />
       </Modal>
     </div>
   );
--- src/LastBird.js
+++ src/LastBird.js
@@ -1,24 +1,4 @@
 function LastBird(props){
-    function showAd(fields) {
-        return (
-            <div className="Ad">
-                <h1>{fields.InitBird.Name}</h1>
-                <div className="info">
-                    <div className="PreviewImage">
-                        <img src={fields.MiddleBird.Image} />
-                    </div>
-                    Sex: {fields.InitBird.Sex}
-                    <br />
-                    How much more adorable than cute: {fields.MiddleBird.Cuteness}%
-                    <br />
-                    Type of friend: {fields.MiddleBird.Friendtype}
-                    <br />
-                    Needs to leave before {fields.MiddleBird.Date}
-                </div>
-            </div>
-        )
-    }
-
     function onChange(e) {
         console.log(e);
         props.saveFields("LastBird", "Acknowledge", e.target.checked);
@@ -30,7 +10,7 @@ function LastBird(props){
             <span className="Instructions">Confirm that this is the ad you want. </span>
             <br />
             <form className="Last-bird">
-                {showAd(props.values)}
+                {props.showAd(props.values)}
                 <br />
                 <input type="checkbox" id="acknowledge" value="acknowledge" 
                        checked={!!props.values?.LastBird?.Acknowledge}
